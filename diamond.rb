414055d9d1d88a1cb074963df77a5114f3b59a5c
Kevin R. Barnes <vinbarnes@gmail.com>
2014-11-30 15:55:26 -0600

    refactor a bit to simplify

--- diamond.rb
+++ diamond.rb
@@ -1,21 +1,23 @@
 class Diamond
   ALPHABET = %w[A B C D E F G H I J K L M N O P Q R S T U V W X Y Z]
-  attr_reader :target, :letters, :rows
+  attr_reader :target, :letters
 
   def initialize(letter)
     @target = letter
     @letters = ("A"..@target).to_a
-    @rows = []
   end
 
-  def build_diamond
-    @rows = if target == "A"
+  def rows
+    @rows ||= build_rows
+  end
+
+  def build_rows
+    if target == "A"
       [letters]
     end
   end
 
   def to_a
-    build_diamond
     rows
   end
 end
